5ad435f01697cfd2579408fd3046a3d43770cf07
Gustavo Henrique Conci <gustavo.conci@a2c.com.br>
2016-10-13 17:39:36 -0300

    fixed bug in IE

--- js/picture.js
+++ js/picture.js
@@ -1,58 +1,62 @@
 (function() {
 
-  var pictures = document.getElementsByTagName('picture'),
-      test = {
-        picture: 'HTMLPictureElement' in window,
-        source: 'HTMLSourceElement' in window,
-        srcset: 'srcset' in document.createElement('source')
-      };
+	var pictures = document.getElementsByTagName('picture'),
+			test = {
+				picture: 'HTMLPictureElement' in window,
+				source: 'HTMLSourceElement' in window,
+				srcset: 'srcset' in document.createElement('source')
+			};
 
-  if (test.picture && test.source && test.srcset) {
-    return false;
-  }
+	if (test.picture && test.source && test.srcset) {
+		return false;
+	}
 
-  function renderPicture() {
+	function renderPicture() {
 
-    var i = -1, picture,
-      picturesLength = pictures.length, img,
-      sources, source, j, sourcesLength, media, mM, matched;
+		var i = -1, picture,
+			picturesLength = pictures.length, img,
+			sources, source, j, sourcesLength, media, mM, matched;
 
-    while (++i < picturesLength) {
+		while (++i < picturesLength) {
 
-      picture = pictures[i];
-      img = picture.getElementsByTagName('img')[0];
-      img.setAttribute('data-src', img.getAttribute('src'));
-      sources = picture.getElementsByTagName('source');
+			picture = pictures[i];
+			img = picture.getElementsByTagName('img')[0];
+			sources = picture.getElementsByTagName('source');
 
-      j = -1;
-      matched = false;
-      sourcesLength = sources.length;
-      while (++j < sourcesLength) {
+			j = -1;
+			matched = false;
+			sourcesLength = sources.length;
+			while (++j < sourcesLength) {
 
-        source = sources[j];
+				source = sources[j];
 
-        if (source.getAttribute('media') != media) {
-          media = source.getAttribute('media');
-          mM = window.matchMedia(media);
-        }
+				if (source.getAttribute('media') != media) {
+					media = source.getAttribute('media');
+					mM = window.matchMedia(media);
+				}
 
-        if (mM.matches) {
-          matched = true;
-          img.setAttribute('src', source.getAttribute('srcset'));
-          break;
-        }
+				if (mM.matches) {
+					matched = true;
 
-      }
+					if (!img.getAttribute('data-src')) {
+						img.setAttribute('data-src', img.getAttribute('src'));
+					}
 
-      if (!matched) {
-        img.setAttribute('src', img.getAttribute('data-src'));
-      }
+					img.setAttribute('src', source.getAttribute('srcset'));
+					break;
+				}
 
-    }
+			}
 
-  }
+			if (!matched && img.getAttribute('data-src')) {
+				img.setAttribute('src', img.getAttribute('data-src'));
+			}
 
-  renderPicture();
-  window.onresize = renderPicture();
+		}
+
+	}
+
+	renderPicture();
+	window.addEventListener('resize', renderPicture);
 
 })();
